3092f49fd86cd85014a01000086001c1a951ee86
jpthalman <jpthalman@gmail.com>
2017-01-20 15:22:26 -0700

    Modify probability of augmentation in augment_image and add LOTs of documentation

--- Projects/BehavioralCloning/utils.py
+++ Projects/BehavioralCloning/utils.py
@@ -9,7 +9,7 @@ from decorators import n_images
 
 def load_data(file):
     """
-    Opens the driving_log.csv and returns the center, left, right, and steering in a dictionary.
+    Opens driving_log.csv and returns center, left, right, and steering in a dictionary.
 
     :param file: Full file path to driving_log.csv
     :type file: String
@@ -29,16 +29,23 @@ def load_data(file):
 
 def keep_n_percent_of_data_where(data, values, condition_lambda, percent):
     """
-    Keeps n-percent of a dataset-values pair where a condition over the values is true.
+    Keeps n-percent of a dataset where a condition over the values is true.
+
+    For example, if you want to remove 90% of the samples in a dataset where the steering angles are
+    close to zero, the code would look like this:
+
+        images, angles = keep_n_percent_of_data_where(images, angles, lambda x: abs(x) < 1e-5, 0.1)
+
+    Note that the lambda should return True for the values you would like to filter.
 
     :param data: The dataset
     :param values: The values to filter over
     :param condition_lambda: A lambda by which to filter the dataset
-    :param percent: The percent of the dataset-value pair (where `condition_lambda` is true) to KEEP. [0.0, 1.0].
+    :param percent: The percent of the dataset-value pair (where `condition_lambda` is true) to KEEP
 
     :type data: np.ndarray
     :type values: np.ndarray
-    :type condition_lambda: lambda
+    :type condition_lambda: lambda or function
     :type percent: float
 
     :return: Filtered tuple: (filtered_data, filtered_values)
@@ -63,9 +70,17 @@ def keep_n_percent_of_data_where(data, values, condition_lambda, percent):
 
 
 def split_data(features, labels, test_size=0.2, shuffle_return=True):
+    """
+    Splits the dataset and labels into training and testing sets, with proportions (1-test_size) and test_size.
+
+    :param features: The dataset to split
+    :param labels: The labels for the dataset
+    :param test_size: The proportion of the dataset to siphon into the test set
+    :param shuffle_return: If True, shuffles the dataset and labels before splitting
+    :return: (X_train, X_test, y_train, y_test)
+    """
     if shuffle_return:
         features, labels = shuffle(features, labels)
-
     return train_test_split(features, labels, test_size=test_size)
 
 
@@ -75,6 +90,9 @@ def process_image(im):
 
     :param im: Image to normalize
     :return: Normalized image with shape (h, w, 1)
+
+    :type im: np.ndarray with shape (h, w, 3)
+    :rtype: np.ndarray with shape (h, w, 1)
     """
     assert im.ndim == 3 and im.shape[2] == 3, 'Must be a BGR image with shape (h, w, 3)'
 
@@ -86,7 +104,7 @@ def process_image(im):
     return im
 
 
-@n_images
+@n_images  # Decorator to generalize single image method to multiple images
 def flip_image(image, angle):
     """
     Mirrors the image from left to right and flips the sign of the angle.
@@ -115,14 +133,14 @@ def val_augmentor(ims, vals):
            np.concatenate((vals, flipped_vals), axis=0)
 
 
-@n_images
+@n_images  # Decorator to generalize single image method to multiple images
 def augment_image(image, value, prob, im_normalizer=process_image):
     """
     Augments an image and steering angle with probability `prob`.
 
     This technique randomly adjusts the brightness, occludes the image with 30 random black squares,
     and slightly rotates, shifts and scales the image. These augmentations are meant to make the
-    model more robust to differing conditions than those in the training set.
+    model more robust to conditions different to those in the training set.
 
     :param image: The image to augment
     :param value: The steering angle associated with the image
@@ -142,22 +160,21 @@ def augment_image(image, value, prob, im_normalizer=process_image):
 
     h, w = image.shape[:2]
 
-    random_uniform = np.random.uniform(0.0, 1.0)
-
-    # Flip the image and angle with probability (prob/2), aka 50% of the time.
-    if random_uniform < prob/2:
+    # Flip the image and angle half the time. Effectively doubles the size of
+    # the dataset while balancing the left and right turn proportions.
+    if np.random.uniform(0.0, 1.0) < 0.5:
         image, value = cv2.flip(image, 1), -value
         image = image[..., None]
 
     # Return un-augmented image and value with probability (1-prob)
-    if random_uniform > prob:
+    if np.random.uniform(0.0, 1.0) > prob:
         return image, value
 
     # Random brightness adjustment
     image += np.random.uniform(-0.3, 0.3)
 
     # Random occlusion with dark squares
-    sq_w, sq_h, sq_count = 25, 25, 30
+    sq_w, sq_h, sq_count = int(0.15*h), int(0.15*h), 30
     for i in range(sq_count):
         pt1 = (np.random.randint(0, w), np.random.randint(0, h))
         pt2 = (pt1[0] + sq_w, pt1[1] + sq_h)
@@ -200,97 +217,48 @@ def augment_image(image, value, prob, im_normalizer=process_image):
     return augmented, value
 
 
-# TODO: Delete if `augment_image` with decorator works
-def augment_set(data, values, prob):
+def batch_generator(ims, angs, batch_size, augmentor, path, kwargs={}, validation=False):
     """
-    Applies `augment_image` to all images in a given batch.
+    Continuously generates batches from the provided images paths and angles.
 
-    :param data: Batch of images
-    :param values: Batch of corresponding values
-    :param prob: The probability of augmenting an image in the batch
-    :return: Tuple containing (augmented_images, augmented_values)
+    This method follows this process. Generates a batch of size `batch_size` in sequential order through
+    the data. It is important that the data is shuffled prior to being fed into the generator. If the
+    dataset is not evenly divisible by the batch size, their will be an orphan batch at the end. To
+    address this, data is randomly selected to fill the batch. This option can be disabled by setting
+    `validation` to True. Once the batch has been constructed, the images are loaded from the image paths
+    using `cv2.imread`. Note that this means the images will be read in BGR format. Lastly, the images
+    and angles are fed into the provided augmentation function, `augmentor`, and then yielded.
 
-    :type data: np.ndarray with shape (N, h, w, ch)
-    :type values: np.ndarray with shape (N,)
-    :type prob: float between [0.0, 1.0]
-    ":rtype: (np.ndarray, np.ndarray)
+    :param ims: The filepaths to the images
+    :param angs: The steering angles corresponding to the image paths
+    :param batch_size: The size of batches to generate
+    :param augmentor: A function which takes inputs (images, angles, **kwargs)
+                      and returns (images, angles)
+    :param path: A filepath which will be appended to the beginning of each image path
+    :param kwargs: A dictionary containing any additional argument-value pairs for the
+                   `augmentor` function
+    :param validation: A boolean indicating whether or not to expand the orphan batch
+                       from the generator. See description above.
+    :return: Generator producing an infinite number of batches adhering to the above policies
     """
-    n_obs = data.shape[0]
-    assert n_obs == values.shape[0], 'Different # of data and labels.'
-
-    aug_batch_data, aug_batch_vals = [], []
-    for i, img in enumerate(data):
-        aug_data, aug_val = augment_image(img, values[i], prob)
-        aug_batch_data.append(aug_data)
-        aug_batch_vals.append(aug_val)
-    return np.array(aug_batch_data), np.array(aug_batch_vals)
-
-
-def batch_generator(ims, vals, batch_size, augmentor, path, args={}):
     n_obs = ims.shape[0]
-    assert n_obs == vals.shape[0], 'Different # of data and labels.'
+    assert n_obs == angs.shape[0], 'Different # of data and labels.'
 
     while True:
         for batch in range(0, n_obs, batch_size):
             next_idx = batch + batch_size
             batch_x = ims[batch:min(next_idx, n_obs), ...]
-            batch_y = vals[batch:min(next_idx, n_obs), ...]
+            batch_y = angs[batch:min(next_idx, n_obs), ...]
 
             # Ensure consistent batch size by adding random images to the last
-            # batch iff n_obs%batch_size != 0.
-            if next_idx > n_obs:
+            # batch iff n_obs % batch_size != 0.
+            if next_idx > n_obs and not validation:
                 rand_idx = np.random.randint(0, n_obs-1, next_idx - n_obs)
                 batch_x = np.concatenate((batch_x, ims[rand_idx, ...]), axis=0)
-                batch_y = np.concatenate((batch_y, vals[rand_idx, ...]), axis=0)
+                batch_y = np.concatenate((batch_y, angs[rand_idx, ...]), axis=0)
 
             # Load the images from their paths
             batch_x = np.array([cv2.imread(path + im) for im in batch_x])
             # Augment the images with the given function
-            batch_x, batch_y = augmentor(batch_x, batch_y, **args)
-            yield batch_x, batch_y
-
-
-# TODO: Delete if `batch_generator` works
-class BatchGenerator(object):
-    def __init__(self, batch_size, load=False, path=''):
-        self.batch_size = batch_size
-        self.load = load
-        self.path = path
-
-    def validation(self, data, values, augmentor=None):
-        n_obs = data.shape[0]
-        assert n_obs == values.shape[0], 'Different # of data and labels.'
-
-        while True:
-            for batch in range(0, n_obs, self.batch_size):
-                batch_x = data[batch:min(batch + self.batch_size, n_obs), ...]
-                batch_y = values[batch:min(batch + self.batch_size, n_obs)]
-
-                if self.load:
-                    batch_x = np.array([cv2.imread(self.path + im) for im in batch_x])
-
-                batch_x = np.array([normalize_image(im) for im in batch_x])
-                yield batch_x, batch_y
-
-                if augmentor is not None:
-                    batch_x, batch_y = augmentor(np.array(batch_x), np.array(batch_y))
-                    yield batch_x, batch_y
-
-    def keras(self, data, values, augmentor=None, args={}):
-        n_obs = data.shape[0]
-        assert n_obs == values.shape[0], 'Different # of data and labels.'
-
-        while True:
-            batch_x, batch_y = [], []
-            for _ in range(self.batch_size):
-                idx = np.random.randint(0, n_obs-1)
-                batch_x.append(data[idx, ...])
-                batch_y.append(values[idx])
-
-            batch_x, batch_y = np.array(batch_x), np.array(batch_y)
-
-            if self.load:
-                batch_x = np.array([cv2.imread(self.path + im) for im in batch_x])
-            if augmentor is not None:
-                batch_x, batch_y = augmentor(np.array(batch_x), np.array(batch_y), **args)
+            batch_x, batch_y = augmentor(batch_x, batch_y, **kwargs)
             yield batch_x, batch_y
