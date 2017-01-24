caec7f75b789e6f0cd2f99c31a4a298d17a067d5
jpthalman <jpthalman@gmail.com>
2017-01-24 07:15:01 -0700

    Try new model structures

--- Projects/BehavioralCloning/model.py
+++ Projects/BehavioralCloning/model.py
@@ -9,13 +9,16 @@ Dependencies:
 
 
 from collections import namedtuple
-
 import numpy as np
+import os
+
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Flatten, Lambda
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.convolutional import Convolution2D
-from keras.layers.advanced_activations import ELU
+from keras.regularizers import l2
+from keras.layers.advanced_activations import ELU, PReLU
+from keras.layers import Activation
 from keras.optimizers import adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 
@@ -30,6 +33,8 @@ __email__ = 'jpthalman@gmail.com'
 Parameters = namedtuple('Parameters', [
     # General settings
     'batch_size', 'max_epochs', 'path', 'angle_shift',
+    # Model settings
+    'l2_reg', 'keep_prob',
     # Optimizer settings
     'learning_rate', 'epsilon', 'decay',
     # Training settings
@@ -38,20 +43,22 @@ Parameters = namedtuple('Parameters', [
 
 params = Parameters(
     # General settings
-    batch_size=64, max_epochs=100, path='', angle_shift=0.1,
+    batch_size=64, max_epochs=100, path='', angle_shift=0.15,
+    # Model settings
+    l2_reg=0.0, keep_prob=0.5,
     # Optimizer settings
-    learning_rate=1e-3, epsilon=1e-8, decay=0.0,
+    learning_rate=1e-2, epsilon=1e-8, decay=0.0,
     # Training settings
-    min_delta=1e-4, patience=10, kwargs={'prob': 0.8}
+    min_delta=1e-4, patience=10, kwargs={'prob': 0.6}
   )
 
 
-# udacity_paths, udacity_angs = utils.concat_all_cameras(
-#     data=utils.load_data('/home/japata/sharefolder/CarND/Projects/BehavioralCloning/UdacityData/', 'driving_log.csv'),
-#     angle_shift=params.angle_shift,
-#     condition_lambda=lambda x: abs(x) < 1e-5,
-#     keep_percent=0.15
-#   )
+udacity_paths, udacity_angs = utils.concat_all_cameras(
+    data=utils.load_data('/home/japata/sharefolder/CarND/Projects/BehavioralCloning/UdacityData/', 'driving_log.csv'),
+    angle_shift=params.angle_shift,
+    condition_lambda=lambda x: abs(x) < 1e-5,
+    keep_percent=0.15
+  )
 
 
 path = '/home/japata/sharefolder/CarND/Projects/BehavioralCloning/Data/'
@@ -62,7 +69,7 @@ center_ims, center_angs = utils.concat_all_cameras(
     data=utils.load_data(path + 'Center/', 'driving_log.csv'),
     angle_shift=params.angle_shift,
     condition_lambda=lambda x: abs(x) < 1e-5,
-    keep_percent=0.1
+    keep_percent=0.15
   )
 
 # Load the data from the left runs
@@ -72,7 +79,7 @@ center_ims, center_angs = utils.concat_all_cameras(
 left_ims, left_angs = utils.concat_all_cameras(
     data=utils.load_data(path + 'Left/', 'driving_log.csv'),
     angle_shift=params.angle_shift,
-    condition_lambda=lambda x: x < 0.0,
+    condition_lambda=lambda x: x < 1e-5,
     keep_percent=0.0
   )
 
@@ -83,19 +90,19 @@ left_ims, left_angs = utils.concat_all_cameras(
 right_ims, right_angs = utils.concat_all_cameras(
     data=utils.load_data(path + 'Right/', 'driving_log.csv'),
     angle_shift=params.angle_shift,
-    condition_lambda=lambda x: x > 0.0,
+    condition_lambda=lambda x: x > -1e-5,
     keep_percent=0.0
   )
 
 # Aggregate all sets into one.
-filtered_images = np.concatenate((center_ims, right_ims, left_ims), axis=0)
-filtered_angles = np.concatenate((center_angs, right_angs, left_angs), axis=0)
+filtered_paths = np.concatenate((center_ims, right_ims, left_ims), axis=0)
+filtered_angs = np.concatenate((center_angs, right_angs, left_angs), axis=0)
 
 # Split the data into training and validation sets
 train_paths, val_paths, train_angles, val_angles = utils.split_data(
-    features=filtered_images,
-    labels=filtered_angles,
-    test_size=0.2,
+    features=filtered_paths,
+    labels=filtered_angs,
+    test_size=0.12,
     shuffle_return=True
   )
 print('Training size: %d | Validation size: %d' % (train_paths.shape[0], val_paths.shape[0]))
@@ -103,40 +110,48 @@ print('Training size: %d | Validation size: %d' % (train_paths.shape[0], val_pat
 
 # Model construction
 model = Sequential([
-    Lambda(lambda x: x/255. - 0.5, input_shape=(66, 200, 3)),
+    Lambda(lambda x: x/255. - 0.5, input_shape=(66, 200, 1)),
 
-    Convolution2D(3, 1, 1, border_mode='valid', init='he_normal', input_shape=(66, 200, 3)),
+    Convolution2D(3, 1, 1, border_mode='valid', init='he_normal'),
 
-    Convolution2D(24, 5, 5, border_mode='valid', init='he_normal', input_shape=(66, 200, 3)),
+    Convolution2D(24, 5, 5, border_mode='valid', init='he_normal'),
+    # 62x194x24
     MaxPooling2D(pool_size=(2,2), border_mode='valid'),
-    ELU(),
+    Activation('relu'),
 
-    Convolution2D(36, 5, 5, border_mode='valid', init='he_normal', input_shape=(31, 98, 24)),
-    MaxPooling2D(pool_size=(2, 2), border_mode='same'),
-    ELU(),
+    # 31x98x24
+    Convolution2D(36, 5, 5, border_mode='valid', init='he_normal'),
+    # 27x94x36
+    MaxPooling2D(pool_size=(2,2), border_mode='same'),
+    Activation('relu'),
 
-    Convolution2D(48, 5, 5, border_mode='valid', init='he_normal', input_shape=(14, 47, 36)),
-    MaxPooling2D(pool_size=(2, 2), border_mode='same'),
-    ELU(),
+    # 13x47x36
+    Convolution2D(48, 5, 5, border_mode='valid', init='he_normal'),
+    # 9x43x48
+    MaxPooling2D(pool_size=(2,2), border_mode='same'),
+    Activation('relu'),
 
-    Convolution2D(64, 3, 3, border_mode='valid', init='he_normal', input_shape=(5, 22, 48)),
-    ELU(),
+    # 5x22x48
+    Convolution2D(64, 3, 3, border_mode='valid', init='he_normal'),
+    Activation('relu'),
 
-    Convolution2D(64, 3, 3, border_mode='valid', init='he_normal', input_shape=(3, 20, 64)),
-    ELU(),
+    # 3x20x64
+    Convolution2D(64, 3, 3, border_mode='valid', init='he_normal'),
+    Activation('relu'),
 
+    # 1x18x64
     Flatten(),
 
     Dense(100, init='he_normal'),
-    ELU(),
+    Activation('relu'),
 
     Dense(50, init='he_normal'),
-    ELU(),
+    Activation('relu'),
 
     Dense(10, init='he_normal'),
-    ELU(),
+    Activation('relu'),
 
-    Dense(1)
+    Dense(1, init='he_normal')
 ])
 
 optimizer = adam(
@@ -148,9 +163,17 @@ model.compile(optimizer=optimizer, loss='mse')
 
 print('\n', model.summary(), '\n')
 
-# Save model structure
-with open('model.json', 'w') as file:
-    file.write(model.to_json())
+
+# Clear TensorBoard Logs
+for file in os.listdir('./logs/'):
+    os.remove('./logs/' + file)
+
+# Remove previous model files
+try:
+    os.remove('model.json')
+    os.remove('model.h5')
+except FileNotFoundError:
+    pass
 
 
 # Model training
@@ -164,7 +187,7 @@ callbacks = [
 model.fit_generator(
     generator=utils.batch_generator(ims=train_paths, angs=train_angles, batch_size=params.batch_size,
                                     augmentor=utils.augment_image, path=params.path, kwargs=params.kwargs),
-    samples_per_epoch=400*params.batch_size,
+    samples_per_epoch=800*params.batch_size,
     nb_epoch=params.max_epochs,
     # Halve the batch size, as `utils.val_augmentor` doubles the batch size by flipping the images and angles
     validation_data=utils.batch_generator(ims=val_paths, angs=val_angles, batch_size=params.batch_size//2,
@@ -174,3 +197,7 @@ model.fit_generator(
     nb_val_samples=2*val_paths.shape[0],
     callbacks=callbacks
   )
+
+# Save model structure
+with open('model.json', 'w') as file:
+    file.write(model.to_json())
