caec2c0d179a172531654ad466f3e3e19b233b84
Jacob <jpthalman@gmail.com>
2017-02-05 13:19:09 -0700

    Small bug fixes

--- Projects/AdvLaneLines/processing.py
+++ Projects/AdvLaneLines/processing.py
@@ -4,7 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-def binary_colorspace_threshold(im, color_space, channel, thresholds):
+def colorspace_threshold(im, color_space, channel, thresholds):
     """
     Returns a binary heatmap of a transformed color channel of an image.
 
@@ -25,19 +25,24 @@ def binary_colorspace_threshold(im, color_space, channel, thresholds):
     else:
         color_ch = new_color_space
 
-    color_ch = np.uint8(255*color_ch/np.max(color_ch))
+    color_ch = np.uint8(255.*color_ch/np.max(color_ch))
     lower, upper = thresholds
 
     binary_output = np.zeros_like(color_ch)
-    binary_output[(color_ch > lower) & (color_ch <= color_ch)] = 1
+    binary_output[(color_ch > lower) & (color_ch <= upper)] = 1
     return binary_output
 
 
-def sobel_gradient_threshold(im, method, kernel_size, thresholds):
-    assert im.ndim == 2, "Argument 'im' must be a single color channel."
+def gradient_threshold(im, color_space, channel, method, kernel_size, thresholds):
+    new_color_space = cv2.cvtColor(im, color_space)
 
-    sobelx = np.abs(cv2.Sobel(im, cv2.CV_64F, 1, 0, ksize=kernel_size))
-    sobely = np.abs(cv2.Sobel(im, cv2.CV_64F, 0, 1, ksize=kernel_size))
+    if channel is not None:
+        color_ch = new_color_space[..., channel]
+    else:
+        color_ch = new_color_space
+
+    sobelx = np.abs(cv2.Sobel(color_ch, cv2.CV_64F, 1, 0, ksize=kernel_size))
+    sobely = np.abs(cv2.Sobel(color_ch, cv2.CV_64F, 0, 1, ksize=kernel_size))
 
     if method == 'x':
         operator = sobelx
@@ -51,13 +56,15 @@ def sobel_gradient_threshold(im, method, kernel_size, thresholds):
         raise ValueError("Argument 'method' must be 'x', 'y', 'm', or 'd'.")
 
     lower, upper = thresholds
+    operator = np.uint8(255. * operator / np.max(operator))
+
     binary_output = np.zeros_like(operator)
     binary_output[(operator > lower) & (operator <= upper)] = 1
     return binary_output
 
 
 def calibrate_camera(cal_ims_dir, nx, ny):
-    cwd = os.getcwd()
+    path = os.getcwd() + cal_ims_dir
     shape = None
     objpoints = []
     imgpoints = []
@@ -65,8 +72,8 @@ def calibrate_camera(cal_ims_dir, nx, ny):
     objp = np.zeros((nx*ny, 3), np.float32)
     objp[:, :2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
 
-    for im_path in os.listdir(cwd + cal_ims_dir):
-        im = cv2.imread(im_path)
+    for im_path in os.listdir(path):
+        im = cv2.imread(path + im_path)
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         shape = gray.shape
         ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
