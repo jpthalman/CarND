36fe49598ebbdf74b25d3cdadd00506c220498d0
Jacob <jpthalman@gmail.com>
2017-02-05 14:28:25 -0700

    Start sliding window

--- Projects/AdvLaneLines/pipeline.py
+++ Projects/AdvLaneLines/pipeline.py
@@ -4,12 +4,12 @@ import os
 import matplotlib.pyplot as plt
 
 from processing import calibrate_camera, undistort_img, colorspace_threshold, gradient_threshold, \
-                       transform_perspective
+                       transform_perspective, histogram
 
 
 ret, M, dist, rvecs, tvecs = calibrate_camera('\\camera_cal\\', 9, 6)
 
-test_im = os.getcwd() + '\\test_images\\test6.jpg'
+test_im = os.getcwd() + '\\test_images\\test4.jpg'
 im = cv2.imread(test_im)
 im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 undistorted = undistort_img(im, M, dist)
@@ -20,7 +20,7 @@ x_grad = gradient_threshold(undistorted, cv2.COLOR_RGB2HSV, 2, 'x', 3, (10, 30))
 thresh_img = np.zeros_like(x_grad)
 thresh_img[(x_grad == 1) | (color_thresh == 1)] = 1
 
-plt.imshow(thresh_img, cmap='gray')
+# plt.imshow(thresh_img, cmap='gray')
 
 ymax, xmax = thresh_img.shape[:2]
 x_shift = 80
@@ -40,3 +40,9 @@ dst = np.float32([
   ])
 
 top_down = transform_perspective(thresh_img, (xmax, ymax), src, dst)
+plt.imshow(top_down, cmap='gray')
+hist, lmax, rmax = histogram(top_down)
+plt.plot(hist)
+print(lmax, rmax)
+
+
