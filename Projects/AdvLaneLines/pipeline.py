59eb598cd69966cb5fb8bcf636a3a10ae03f2acd
Tesseract7 <jpthalman@gmail.com>
2017-02-06 07:17:07 -0700

    Add lane lines and de-warp

--- Projects/AdvLaneLines/pipeline.py
+++ Projects/AdvLaneLines/pipeline.py
@@ -4,12 +4,12 @@ import os
 import matplotlib.pyplot as plt
 
 from processing import calibrate_camera, undistort_img, colorspace_threshold, gradient_threshold, \
-                       transform_perspective, histogram
+                       transform_perspective, histogram, sliding_window
 
 
 ret, M, dist, rvecs, tvecs = calibrate_camera('\\camera_cal\\', 9, 6)
 
-test_im = os.getcwd() + '\\test_images\\test4.jpg'
+test_im = os.getcwd() + '\\test_images\\test5.jpg'
 im = cv2.imread(test_im)
 im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 undistorted = undistort_img(im, M, dist)
@@ -25,10 +25,10 @@ thresh_img[(x_grad == 1) | (color_thresh == 1)] = 1
 ymax, xmax = thresh_img.shape[:2]
 x_shift = 80
 src = np.float32([
-    [150, 675],
+    [0, 675],
     [xmax//2 - x_shift, 450],
     [xmax//2 + x_shift, 450],
-    [xmax-150, 675]
+    [xmax, 675]
   ])
 
 shift = 100
@@ -40,9 +40,30 @@ dst = np.float32([
   ])
 
 top_down = transform_perspective(thresh_img, (xmax, ymax), src, dst)
+left_fit, right_fit = sliding_window(top_down, 9)
+
+ploty = np.arange(ymax, dtype=np.float32)
+left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
 plt.imshow(top_down, cmap='gray')
-hist, lmax, rmax = histogram(top_down)
-plt.plot(hist)
-print(lmax, rmax)
+plt.plot(left_fitx, ploty, color='blue')
+plt.plot(right_fitx, ploty, color='blue')
 
+warp_zero = np.zeros_like(top_down).astype(np.uint8)
+color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
 
+# Recast the x and y points into usable format for cv2.fillPoly()
+pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+pts = np.hstack((pts_left, pts_right))
+
+# Draw the lane onto the warped blank image
+cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+# Warp the blank back to original image space using inverse perspective matrix (Minv)
+newwarp = transform_perspective(color_warp, (color_warp.shape[1], color_warp.shape[0]), dst, src)
+# Combine the result with the original image
+print(undistorted.shape, newwarp.shape)
+result = cv2.addWeighted(undistorted, 1, newwarp, 0.3, 0)
+plt.imshow(result)
