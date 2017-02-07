b8cae0f8678829f90fecdc743856147d06340bdf
jpthalman <jpthalman@gmail.com>
2017-02-07 11:39:56 -0700

    Separate validity checks into their own module

---
+++ Projects/AdvLaneLines/checks.py
@@ -0,0 +1,14 @@
+"""
+The purpose of these functions are to check the validity of the predicted lane lines. They must have a
+boolean return and should perform one check per function.
+"""
+
+import numpy as np
+import cv2
+
+
+def roughly_parallel(left, right, percent):
+    similar = True
+    for l_coeff, r_coeff in zip(map(abs, left[:2]), map(abs, right[:2])):
+        similar &= abs(l_coeff - r_coeff)/max(l_coeff, r_coeff) < percent
+    return similar
