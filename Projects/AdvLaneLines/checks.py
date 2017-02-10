247a61a5837451d7d274e6146a1a2d72ad5594bd
jpthalman <jpthalman@gmail.com>
2017-02-10 13:19:38 -0700

    Add check for same line and comments

--- Projects/AdvLaneLines/checks.py
+++ Projects/AdvLaneLines/checks.py
@@ -8,6 +8,14 @@ import cv2
 
 
 def roughly_parallel(left, right, percent):
+    """
+    If the two curves non-linear coefficients are similar enough, return true.
+
+    :param left: Left curve
+    :param right: Right curve
+    :param percent: Maximum divergence of the lines
+    :return: Boolean
+    """
     similar = True
     for l_coeff, r_coeff in zip(map(abs, left[:2]), map(abs, right[:2])):
         similar &= abs(l_coeff - r_coeff)/max(l_coeff, r_coeff) < percent
@@ -15,6 +23,25 @@ def roughly_parallel(left, right, percent):
 
 
 def similar_curvature(left, right, percent):
+    """
+    Measures the similarity of the two curvatures.
+
+    :param left: Left curvature
+    :param right: Right curvature
+    :param percent: Max percent divergence
+    :return: Boolean
+    """
     min = np.min([left, right])
     max = np.max([left, right])
     return min/max > percent if max < 1000 else True
+
+
+def not_same_line(left, right):
+    """
+    Ensures that the two lane lines are not the same line, indicating failure.
+
+    :param left: Left line
+    :param right: Right line
+    :return: Boolean
+    """
+    return abs(left[2] - right[2]) > 1e-2
